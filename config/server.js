d78d35da64f8d695a844d325dfc2ffe0220b8139
ayush_tibra <ayush.tibra@growisto.com>
2021-05-19 13:02:39 +0530

    initial commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '9b3d4492a1785fd39cb996c2edad97b3'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '906d8c82-c3d8-450f-a802-6d4d65817bf4'
+};
